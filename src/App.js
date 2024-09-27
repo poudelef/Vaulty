f0e50ad25b3442434bdd73b84744f74044623812
sambhavpoudel88@gmail.com <youremail@example.com>
2024-09-27 16:29:48 -0400

    Updating code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import Profile_img from './assets/profile.jpg';
 import './App.css';
+import profile from'./assets/profile.jpg'
 
-
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-
-function Profile(){
-  return(
-    <img
-    src={Profile_img}
-    alt="Profile Picture"
-    className='profile_img '
-    />
-  );
-}
-
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
 
 function App() {
   return (
     <div className='App'>
-      <h1>Welcome to Vaulty {person.name}</h1>
-      <MyButton />
-      <Profile/>
+      
+        <h1>Vaulty</h1>
+    
+      <div className='content'>
+        <aside className='sidebar'>
+        <div class = 'main-content'>
+        <div class = 'one'><button class ='main-content-button'>Play</button></div>
+        <div class = 'one'><button class ='main-content-button'>Next Rotation</button></div>
+        <div class = 'one'><button class ='main-content-button'>Intrest buttonaid</button></div>
+        <div class = 'one'><button class ='main-content-button'>Total Money Received</button></div>
+        <div class = 'one'><button class ='main-content-button'>Will add someting</button></div>
+        </div>
+        </aside>
+        <main className='info'>
+        <div><img src={profile} alt='Profile' className='profile_img' /></div>
+          <p>Name: Sambhav Poudel</p>
+          <p>Contaact: 5132702070</p>
+          <p>Address: 3033 Cliftion Avenue</p>
+          <p>BanK Information: Chase 3453</p>
+          <p>Will add more feature</p>
+        </main>
+      </div>
     </div>
   );
 }
